483207b9516edf3459088502d08e4fb4b92bcb07
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-01-16 10:01:01 +0100

    122. Spring Pet Clinic - Pet Type Data on Startup

--- petclinic-web/src/main/java/mm/springframework/petclinic/bootstrap/DataLoader.java
+++ petclinic-web/src/main/java/mm/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package mm.springframework.petclinic.bootstrap;
 
 import mm.springframework.petclinic.model.Owner;
+import mm.springframework.petclinic.model.PetType;
 import mm.springframework.petclinic.model.Vet;
 import mm.springframework.petclinic.services.OwnerService;
+import mm.springframework.petclinic.services.PetTypeService;
 import mm.springframework.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,14 +17,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petService = petService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatType = petService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
