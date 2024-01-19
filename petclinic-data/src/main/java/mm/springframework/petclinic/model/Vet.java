19f4727dbd15c8b6eb1dbd24a2f9533db1439071
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-01-19 16:11:41 +0100

    147. Spring Pet Clinic - Convert Vets to JPA Entities

--- petclinic-data/src/main/java/mm/springframework/petclinic/model/Vet.java
+++ petclinic-data/src/main/java/mm/springframework/petclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package mm.springframework.petclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specielties", joinColumns = @JoinColumn(name = "vet_id"),
+                inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
