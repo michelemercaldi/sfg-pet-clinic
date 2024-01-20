34001845862138504df7cc1b0c65e4068e277fa1
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-01-20 01:50:59 +0100

    150. Spring Pet Clinic - Add Spring Data JPA Owner Service

--- petclinic-data/src/main/java/mm/springframework/petclinic/repositories/OwnerRepository.java
+++ petclinic-data/src/main/java/mm/springframework/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import mm.springframework.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String name);
 }
